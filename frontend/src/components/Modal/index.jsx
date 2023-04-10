6f8366520df8162263c7604356d4d51cce3f9ed4
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-04-10 11:15:04 -0700

    issue-4:countdown

---
+++ frontend/src/components/Modal/index.jsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion"
+import Backdrop from "../Backdrop"
+
+const Modal = ({ handleClose, text }) => {
+  const dropIn = {
+    hidden: {
+      y: "-100vh",
+      opacity: 0,
+    },
+    visible: {
+      y: "0",
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: "spring",
+        damping: 25,
+        stiffness: 500,
+      },
+    },
+    exit: {
+      y: "100vh",
+      opacity: 0,
+    },
+  }
+
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="modal border-secondary"
+        varients={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit='exit'
+      >
+        <p>{text}</p>
+        <button className="btn btn-secondary" onClick={handleClose}>Close</button>
+      </motion.div>
+    </Backdrop>
+  )
+}
+export default Modal
--- frontend/src/pages/Main/Hero.jsx
+++ frontend/src/pages/Main/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section>
       <div className="dark:bg-violet-400">
-        <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+        <div className="container flex flex-col items-center px-4 py-6 pb-6 mx-auto text-center dark:text-gray-900">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-900">
             <div>
             <Countdown />
@@ -30,7 +30,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      
+
     </section>
   )
 }
